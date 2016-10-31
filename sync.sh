859d15b84ad0ec6dd13130a67663c91d80d5afab
Daniel Kranich <Daniel@nusalaska.com>
2016-10-31 09:41:42 -0800

    Setup the sync to also add a global .gitconfig

--- sync.sh
+++ sync.sh
@@ -5,9 +5,11 @@ git submodule update --init --recursive
 
 rm -rf $HOME/.vi*
 rm -f $HOME/.bash_profile
+rm -f $HOME/.gitconfig
 
 ln -s $dir/.vi* $HOME
 ln -s $dir/.bash* $HOME
+ln -s $dir/.gitconfig $HOME
 
 echo "Please run the following commands"
 echo "sudo yum install ctags the_silver_searcher"
