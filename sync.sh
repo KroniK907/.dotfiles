5f63c5311bb538796730a0dde4bc90d3e5592bb8
Daniel Kranich <Daniel@nusalaska.com>
2016-10-31 09:29:40 -0800

    allowed the sync script to add the virc file

--- sync.sh
+++ sync.sh
@@ -3,10 +3,10 @@ dir=$(pwd)
 
 git submodule update --init --recursive
 
-rm -rf $HOME/.vim*
+rm -rf $HOME/.vi*
 rm -f $HOME/.bash_profile
 
-ln -s $dir/.vim* $HOME
+ln -s $dir/.vi* $HOME
 ln -s $dir/.bash* $HOME
 
 echo "Please run the following commands"
